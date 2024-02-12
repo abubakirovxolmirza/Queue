2de56512afa30f81ed16fc13cb72a7e8031346d2
abubakirovxolmirza <abubakirovxolmirza1@gmail.com>
2024-02-12 12:36:15 +0500

    connect databaes

--- config/settings.py
+++ config/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'config.wsgi.app'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'verceldb',
+            'HOST': 'ep-blue-queen-75951307-pooler.us-east-1.aws.neon.tech',
+            'USER': 'default',
+            'PASSWORD': 'MzTERP0jt7Ol',
+        }
     }
-}
 
 
 # Password validation
